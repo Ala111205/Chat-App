16bcf46318fe8c90a0778ae0d8da175d3442b652
Ala111205 <sadham070403@gmail.com>
2025-10-01 14:10:51 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -173,5 +173,5 @@ async function sendGroupList(ws) {
   ws.send(JSON.stringify({ type: 'joinedGroups', groups: userRooms.map(r => r.name) }));
 }
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
