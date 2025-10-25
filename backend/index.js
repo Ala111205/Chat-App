42aea677a7fbaf7c0eeb52be4fe24efe7f712b7e
Ala111205 <sadham070403@gmail.com>
2025-10-25 13:43:33 +0530

    host

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 const allowedOrigins = [
   'https://chat-app-indol-gamma.vercel.app',
   'http://localhost:3000',
-  'http://127.0.0.1:5500'
+  'http://127.0.0.1:5501'
 ];
 
 const corsOptions = {
